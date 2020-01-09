26059f2ba27175bd72d356d75aabc9561b4e0a20
PAOLO DANIEL BUDIEL ECHEVARRIA <budiel420@hotmail.com>
2020-01-09 16:49:24 -0500

    w2.5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { Promotion } from '../shared/promotion';
+import { DishService } from '../services/dish.service';
+import { PromotionService } from '../services/promotion.service';
 
 @Component({
   selector: 'app-home',
